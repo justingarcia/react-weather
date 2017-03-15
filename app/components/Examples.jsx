a0d4c6ad48d98224ae78f787e97841f4041b88bc
Justin Garcia <justinqgarcia@gmail.com>
2017-03-15 13:42:41 -0700

    Add columns to Main; style Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
     return(
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the Examples Page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Pasadena'>Pasadena, CA</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Tokyo'>Tokyo, Japan</Link>
+                </li>
+            </ol>
         </div>
     );
 };
